6075115b27fb0c73bd5bb62c6a16f73a83abed29
rrmaduzia <rafi11m@hotmail.com>
2020-12-20 15:23:18 +0100

    fixed Pagination result

--- src/main/java/pl/createcompetition/service/query/GetQueryAbstractService.java
+++ src/main/java/pl/createcompetition/service/query/GetQueryAbstractService.java
@@ -15,8 +15,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
 import static java.util.stream.Collectors.toList;
 
 //TODO HAVE TO REFACTOR PAGINATION CODE
@@ -27,15 +25,12 @@ public abstract class GetQueryAbstractService<B extends QueryDtoInterface<R>, R>
 
     protected abstract Predicate getPredicate(Predicate predicate, CriteriaBuilder builder, Root r, List<SearchCriteria> params);
 
-
+    //Page Number start from 0
     public PagedResponseDto<?> execute(Class<B> encja, String search, int pageNumber, int pageSize) {
         final CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         final CriteriaQuery<B> query = builder.createQuery(encja);
         final Root r = query.from(encja);
 
-        //page number zaczyna sie od 1
-
-
         List<SearchCriteria> params = new ArrayList<>();
         if (search != null) {
             Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
@@ -45,95 +40,35 @@ public abstract class GetQueryAbstractService<B extends QueryDtoInterface<R>, R>
             }
         }
 
-
         Predicate predicate = builder.conjunction();
         predicate = getPredicate(predicate, builder, r, params);
         query.where(predicate);
 
         var mapperFirst = new Mapper<B, R>();
 
-
         //TODO HAVE TO REFACTOR PAGINATION CODE
-/*
-        CriteriaQuery<Long> countQuery = builder
-                .createQuery(Long.class);
-        countQuery.select(builder
-                .count(countQuery.from(encja)));
-        Long total_elements = entityManager.createQuery(countQuery)
-                .getSingleResult();
- */
+
+        CriteriaQuery<Long> countQuery = builder.createQuery(Long.class);
+        countQuery.select(builder.count(countQuery.from(encja)));
+
+        int total_elements = Math.toIntExact(entityManager.createQuery(countQuery).getSingleResult());
+        int totalPages = (int) Math.ceil(total_elements / (double) pageSize);
+
 
         TypedQuery typedQuery = entityManager.createQuery(query);
-       // while (pageNumber < total_elements.intValue()) {
-            typedQuery.setFirstResult(pageNumber);
-            typedQuery.setMaxResults(pageSize);
-            //pageNumber += pageSize;
-      //  }
+        typedQuery.setFirstResult(pageNumber);
+        typedQuery.setMaxResults(pageSize);
 
         List<B> typeQueryList = typedQuery.getResultList();
-
-        int total_elements_final = typeQueryList.size();
-
         List<R> rsm = mapperFirst.map(typeQueryList);
 
-
-        PageModel tms = new PageModel(pageNumber,pageSize,total_elements_final,1,true);
-
+        PageModel tms = new PageModel(pageNumber,pageSize,total_elements,totalPages,true);
 
         return PagedResponseDtoBuilder.create()
                 .listDto(rsm)
                 .entityPage(tms)
                 .build();
-
-
-
-       /*
-
-        List<B> returnList = entityManager
-                .createQuery(query)
-                .getResultStream()
-                .collect(Collectors.toList());
-
-
-        return mapperFirst.map(entityManager
-                .createQuery(query)
-                .getResultStream()
-                .collect(Collectors.toList()));
-
-        */
     }
-
-    /*
-    public List<R> execute(Class<B> encja, String search) {
-        final CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-        final CriteriaQuery<B> query = builder.createQuery(encja);
-        final Root r = query.from(encja);
-
-
-        List<SearchCriteria> params = new ArrayList<>();
-        if (search != null) {
-            Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
-            Matcher matcher = pattern.matcher(search + ",");
-            while (matcher.find()) {
-                params.add(new SearchCriteria(matcher.group(1), matcher.group(2), matcher.group(3)));
-            }
-        }
-
-        Predicate predicate = builder.conjunction();
-        predicate = getPredicate(predicate, builder, r, params);
-        query.where(predicate);
-
-        var mapperFirst = new Mapper<B, R>();
-
-        return mapperFirst.map(entityManager
-                .createQuery(query)
-                .getResultStream()
-                .collect(Collectors.toList()));
-    }
-
-
-     */
-
 
     static class Mapper<A extends QueryDtoInterface<B>, B> {
         public List<B> map(List<A> from) {
@@ -142,5 +77,4 @@ public abstract class GetQueryAbstractService<B extends QueryDtoInterface<R>, R>
                     .collect(toList());
         }
     }
-
 }
