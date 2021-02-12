9a9c24c76a33542b8b8b2785dd500ffcc8974419
rrmaduzia <rafi11m@hotmail.com>
2021-02-12 23:38:35 +0100

    formated code

--- src/main/java/pl/createcompetition/service/MatchesInCompetitionService.java
+++ src/main/java/pl/createcompetition/service/MatchesInCompetitionService.java
@@ -19,7 +19,6 @@ public class MatchesInCompetitionService {
     private final GetQueryImplService<MatchInCompetition,?> queryUserDetailService;
     private final VerifyMethodsForServices verifyMethodsForServices;
     private final MatchInCompetitionRepository matchInCompetitionRepository;
-    private final CompetitionRepository competitionRepository;
 
     public PagedResponseDto<?> searchMatchesInCompetition(String search, PaginationInfoRequest paginationInfoRequest) {
 
@@ -40,7 +39,6 @@ public class MatchesInCompetitionService {
         return ResponseEntity.ok(matchInCompetitionRepository.save(matchInCompetition));
     }
 
-
     public ResponseEntity<?> updateMatchesInCompetition(MatchInCompetition matchInCompetition, Long matchId, UserPrincipal userPrincipal) {
 
         checkIfWinnerTeamHasNotBeenApprovedBeforeMatchStarted(matchInCompetition);
@@ -54,8 +52,6 @@ public class MatchesInCompetitionService {
         return ResponseEntity.ok(matchInCompetitionRepository.save(matchInCompetition));
     }
 
-
-
     public ResponseEntity<?> deleteMatchesInCompetition(Long matchId, UserPrincipal userPrincipal) {
 
         MatchInCompetition foundMatch = findMatch(matchId);
@@ -65,7 +61,6 @@ public class MatchesInCompetitionService {
 
         return ResponseEntity.noContent().build();
     }
-
 
     private void checkIfCompetitionBelongToUser(String competitionName, Competition findCompetition) {
         if (!findCompetition.getCompetitionName().equals(competitionName)) {
@@ -84,7 +79,6 @@ public class MatchesInCompetitionService {
                 new ResourceNotFoundException("Match not exists", "Id", matchId));
     }
 
-
     public void checkIfTeamParticipatingInCompetition(String team1, String team2, Competition competition) {
         if (!competition.getTeams().contains(team1)) {
             throw new BadRequestException(team1 + " are not part of competition named: " + competition.getCompetitionName());
@@ -99,8 +93,4 @@ public class MatchesInCompetitionService {
             throw new BadRequestException("You can't set up winner if match wasn't played");
         }
     }
-
-
-
-
 }
