a596b320f351fbc402130d6ab32a0a374fb10e58
rrmaduzia <rafi11m@hotmail.com>
2021-02-15 18:28:13 +0100

    fixed unrecognzied method

--- src/main/java/pl/createcompetition/repository/CompetitionRepository.java
+++ src/main/java/pl/createcompetition/repository/CompetitionRepository.java
@@ -1,21 +1,13 @@
 package pl.createcompetition.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import pl.createcompetition.model.Competition;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface CompetitionRepository extends JpaRepository<Competition, Long>{
-    List<Competition> findAllByCompetitionNameContainingIgnoreCase(String competitionName);
-    List<Competition> findAllByCity(String city);
-    List<Competition> findAllByOwner(String owner);
 
-    //@Query(value = "SELECT competition_name from competition  WHERE competition_name=?1", nativeQuery =true)
     Optional<Competition> findByCompetitionName(String competitionName);
     boolean existsCompetitionByCompetitionNameIgnoreCase(String competitionName);
-
-
-
+    Optional<Competition> findByCompetitionNameAndCompetitionOwner(String competitionName, String competitionOwner);
+    
 }
--- src/main/java/pl/createcompetition/repository/MatchesInTournamentRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.createcompetition.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.createcompetition.model.MatchesInTournament;
-
-public interface MatchesInTournamentRepository extends JpaRepository<MatchesInTournament, Long> {
-}
