e2256ac2e9f1401ea6a7211dde952c483d62d8e1
rrmaduzia <rafi11m@hotmail.com>
2021-02-18 22:29:25 +0100

    changed broker settings

--- src/main/java/pl/createcompetition/config/WebSocketConfig.java
+++ src/main/java/pl/createcompetition/config/WebSocketConfig.java
@@ -23,7 +23,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-      //  config.enableSimpleBroker("/topic/", "queue");
 
         config.enableStompBrokerRelay("/topic", "/queue")
                 .setRelayHost("localhost")
