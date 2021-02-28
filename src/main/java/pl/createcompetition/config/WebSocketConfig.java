3730b6dbe210836821e8903bf3301df4372d0e7c
rrmaduzia <rafi11m@hotmail.com>
2021-02-28 12:43:38 +0100

    changed Socket configuration

--- src/main/java/pl/createcompetition/config/WebSocketConfig.java
+++ src/main/java/pl/createcompetition/config/WebSocketConfig.java
@@ -1,20 +1,11 @@
 package pl.createcompetition.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
-
-import javax.servlet.http.HttpSession;
-import java.util.Map;
-
 
 @Configuration
 @EnableWebSocket
@@ -40,7 +31,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setAllowedOrigins("*")
                 .withSockJS();
     }
-
-
-
 }
